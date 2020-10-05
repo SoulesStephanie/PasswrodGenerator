d2324f0315ec40ceb9defe0cd2bfccf495a0b589
Violet Mae <rideuforfun@gmail.com>
2020-10-04 20:47:09 -0600

    added password length and charater functions and alerts

--- script.js
+++ script.js
@@ -21,3 +21,21 @@ var charListFinal = ""
 var generateButton = document.querySelector("#gen")
 var copyButton = document.querySelector("#copy")
 var passwordBox = document.querySelector("#pass-final")
+
+// Function and alert to make sure user selected password length between 9-110
+
+var choseLengthCorrectly = function () {
+    alert("Please select a password length between 9 and 110 characters.")
+ passwordLength = prompt("How many characters would you like your secure password to be? Needs to be between 9 and 110.")
+ passwordLengthChecker = Number (passwordLength)
+}
+
+// Function and alert to make sure at least one type of character is selected
+var choseCharList = function () {
+    alert("Please select at least one of the four character types.")
+    charTypeSpec = confirm("Would you like to use special characters including a space?")
+    charTypeNumbers = confirm("Would you like to use numbers 0-9?")
+    charTypeLowercase = confirm("Would you like to use lowercase characters?")
+    charTypeUppercase = confirm("Would you like to use uppercase characters?")
+    
+}
