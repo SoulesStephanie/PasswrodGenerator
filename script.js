b9b54edd3e59907721bcdb9da7eed21363c15874
Violet Mae <rideuforfun@gmail.com>
2020-10-04 20:07:16 -0600

    added button variables

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Prompt to ask quicks about the type of chahrtrs and size of passwrod to be generated. 
+// Prompt to ask quicks about the type of chahrtrs and size of password to be generated. 
 alert("Welcome to Steph's Awesome Secure Password Generator!")
 var passwordLength = prompt("How many characters would you like your password to be? Pick a number between 9 and 110.")
 var passwordLengthChecker = Number(passwordLength)
@@ -7,9 +7,17 @@ var charTypeNumbers = confirm("Would you like to use numbers 0-9?")
 var charTypeLowercase = confirm("Would you like to use lowercase letters?")
 var charTypeUppercase = confirm("Would you like to use uppercase letters?")
 
-// Varibales for characters/letters
+// Variables for characters/letters
 
 var specialChars = " !\"/#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
 var numChars = "0123456789"
 var lowerLetters = "abcdefghijklmnopqrstuvwxyz"
 var upperLetters = lowerLetters.toUpperCase()
+
+var charListFinal = ""
+
+// Button Variables
+
+var generateButton = document.querySelector("#gen")
+var copyButton = document.querySelector("#copy")
+var passwordBox = document.querySelector("#pass-final")
